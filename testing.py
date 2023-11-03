0c8a1927fb7aa5846ec8125fcf832f2dcba59d63
TheRad1 <landon.bakken@gmail.com>
2023-11-03 10:46:33 -0500

    other axis are now being read

--- testing.py
+++ testing.py
@@ -3,6 +3,8 @@ import time
 
 leftStick = (0, 0)
 rightStick = (0, 0)
+leftTrigger = 0
+rightTrigger = 0
 
 pygame.init()
 
@@ -29,15 +31,19 @@ try:
                 if event.axis == 0:  # left stick X
                     leftStick = (event.value, leftStick[1])
                 elif event.axis == 1:  # left stick Y
-                    leftStick = (leftStick[0], event.value)
-                elif event.axis == 2:  # right stick X
+                    leftStick = (leftStick[0], -event.value)
+                elif event.axis == 2:  # left trigger
+                    leftTrigger = (event.value + 1) / 2
+                elif event.axis == 3:  # right stick X
                     rightStick = (event.value, rightStick[1])
-                elif event.axis == 3:  # right stick Y
-                    rightStick = (rightStick[0], event.value)
+                elif event.axis == 4:  # right stick Y
+                    rightStick = (rightStick[0], -event.value)
+                elif event.axis == 5:  # right trigger
+                    rightTrigger = (event.value + 1) / 2
         if time.time() - printClock >= printInterval:
             printClock = time.time()
             print(
-                f"Left: ({leftStick[0]:.{3}f},{leftStick[1]:.{3}f})  Right: ({rightStick[0]:.{3}f},{rightStick[1]:.{3}f})"
+                f"\n\n\n\n\n\n\n\n\nLeft Joystick: ({leftStick[0]:.{3}f},{leftStick[1]:.{3}f})\nRight Joystick: ({rightStick[0]:.{3}f},{rightStick[1]:.{3}f})\nRight Trigger: {rightTrigger:.{3}f}\nLeft Trigger: {leftTrigger:.{3}f}"
             )
 except KeyboardInterrupt:
     pass
