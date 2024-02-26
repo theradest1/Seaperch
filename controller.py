fdb6b038db7162fdf724f88d2f805a47190c33f8
TheRad1 <landon.bakken@gmail.com>
2024-02-26 08:41:31 -0600

    added controller

--- controller.py
+++ controller.py
@@ -129,7 +129,7 @@ class controller:
 
     def setAxis(self, axis, value, isAxis=True):
         if (
-            self.name == "PS4 Controller" or self.name == "Controller (Gamepad F310)" or self.name == "Controller (XBOX 360 For Windows)"
+            self.name == "PS4 Controller" or self.name == "Controller (Gamepad F310)" or self.name == "Controller (XBOX 360 For Windows)" or self.name == "Generic X-Box pad"
         ):  # use official name later (and test)
             if isAxis:
                 if axis == 0:  # left stick X
